d5d67a039379cb7834dc5edf136a1688d7ef88fb
KarolBurgos <karburgos@umariana.edu.co>
2018-02-07 09:16:43 -0500

    Diseño de suscribete

--- app/src/main/java/com/example/karolb/protegemosapp/MainActivity.java
+++ app/src/main/java/com/example/karolb/protegemosapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -96,8 +98,35 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
+            AlertDialog.Builder uBuilder2 = new AlertDialog.Builder(this);
+            View aView2 = getLayoutInflater().inflate(R.layout.fragment_suscribirse, null);
+            uBuilder2.setView(aView2);
+            final AlertDialog dialog2 = uBuilder2.create();
+            dialog2.show();
+            Button close = (Button) aView2.findViewById(R.id.close);
+
+            close.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog2.cancel();
+                }
+            });
+
         } else if (id == R.id.nav_share) {
-            fragmentClass=contactenosFragment.class;
+
+            AlertDialog.Builder uBuilder2 = new AlertDialog.Builder(this);
+            View aView2 = getLayoutInflater().inflate(R.layout.fragment_contactenos, null);
+            uBuilder2.setView(aView2);
+            final AlertDialog dialog2 = uBuilder2.create();
+            dialog2.show();
+            Button close = (Button) aView2.findViewById(R.id.close);
+
+            close.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog2.cancel();
+                }
+            });
 
         } else if (id == R.id.nav_send) {
 
